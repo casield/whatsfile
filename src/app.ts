bdc3496f96836b5b4f724ab313acd986c189ece3
Casiel <casield@gmail.com>
2021-05-31 03:00:53 -0500

    all

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-//app.use(cors(corsOptions));
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
